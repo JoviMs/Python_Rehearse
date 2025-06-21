aea879b6091ce38940db3ff263ee3db31e76faea
Junior Nfor <juniornfor1@gmail.com>
2025-06-21 23:01:57 +0200

    Python converter from usd to euro at vice versa

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
-#PYTHON Calculator
+#python currency converter
 
-operator = input("Enter an operator of your choice (+,//,/,-,*): ")
-number1 = float(input("Enter your first number: "))
-number2 = float(input("Enter your second number: "))
+currency = int(input("Enter Currency : "))
+convert = input("From which currency do you want to convert from USD or Euro?: ")
 
-if operator == "+":
-    print(number1 + number2)
-elif operator == "-":
-    print(number1 - number2)
-elif operator == "*":
-    print(number1 * number2)
-elif operator == "/":
-    print(round(number1 / number2, 3))
-elif operator == "//":
-    print(round(number1 // number2))
-elif operator != "-" and operator == "//":
-    print(round,(number1 % number2, 3))
+if convert == "Euro":
+    amount = currency * 1.15
+    print(f"The converted amount from Euros is {amount}usd.")
 else:
-    print("Invalid operator")
+    amount = currency * 0.87
+    print(f"The converted amount from USD is {amount}euro.")
