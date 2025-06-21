6f4356983d6c9ee613f7982bbf19f09fdc002e05
Junior Nfor <juniornfor1@gmail.com>
2025-06-22 00:09:45 +0200

    Update app.py with new changes

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #python currency converter
 
-currency = int(input("Enter Currency : "))
+currency = int(input("How much do you want to convert? : "))
 convert = input("From which currency do you want to convert from USD or Euro?: ")
 
 if convert == "Euro":
     amount = currency * 1.15
     print(f"The converted amount from Euros is {amount}usd.")
-else:
+elif convert == "USD":
     amount = currency * 0.87
     print(f"The converted amount from USD is {amount}euro.")
+else:
+    print(f"You gave a wrong currency for your exchange!")
