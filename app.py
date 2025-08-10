3305b6c180edc97882f813df0ee57e0c67e2c8a9
Junior Nfor <juniornfor1@gmail.com>
2025-08-11 01:25:20 +0200

    handgman, had another file for wordlist

--- app.py
+++ app.py
@@ -301,7 +301,7 @@ def main():
 if __name__ == "__main__":
     main()   
 '''
-
+'''
 import random
 import string
 
@@ -333,3 +333,82 @@ for letter in cipher_text:
     cipher_text += key[index]
 print (f"original message: {cipher_text}")
 print(f"encrypted message: {plain_text}")
+'''
+
+#hangman
+from wordlist import words
+import random
+
+ #dictionary of key
+hangman_art = {0: ("  ",
+                   "  ",
+                   "  "),
+               1:(" 0 ",
+                  "   ",
+                  "   "),
+               2:(" o ",
+                  " | ",
+                  "   "),
+               3:(" o ",
+                  "/| ",
+                  "   "),
+               4:(" o ",
+                  "/|\\",
+                  "   "),
+               5:(" o ",
+                  "/|\\",
+                  "/  ",
+                  ),
+               6:(" o ",
+                  "/|\\",
+                  "/ \\"), }
+
+def display_man(wrong_guesses):
+    for line in hangman_art[wrong_guesses]:
+        print(line)
+def display_hint(hint):
+    print(" ".join(hint))
+def display_answer(answer):
+    print(" ".join(answer))
+def main():
+    answer = random.choice(words)
+    hint = ["_"] * len(answer)
+    wrong_guesses = 0
+    guessed_letters = set()
+    is_running = True
+
+    while is_running:
+        display_man(wrong_guesses)
+        display_hint(hint)
+        guess = input("Enter your guess: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single letter.")
+            continue
+        if guess in guessed_letters:
+            print(f"Your {guess} guessed that letter. Try again.")
+            continue
+            guessed_letters.add(guess)
+
+        if guess in answer:
+           for i in range(len(answer)):
+               if answer[i] == guess:
+                hint[i] = guess
+
+        else:
+            wrong_guesses += 1
+        if "_" not in hint:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("You win! Thank you for playing!")
+            is_running = False
+
+        elif wrong_guesses >= len(hangman_art) - 1:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("Sorry, you didn't win anything.")
+            is_running = False
+
+
+if __name__ == '__main__':
+    main()
