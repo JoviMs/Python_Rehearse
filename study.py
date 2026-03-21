d4c84485c19336a30f0aeb1955a27e0e9ac07494
jn10852 <junior.nfor@stud.th-deg.de>
2026-03-21 01:39:37 +0100

    grade calculator function

--- study.py
+++ study.py
@@ -1,19 +1,16 @@
-# outside - define the function
-def calculate_bmi(weight, height):
-    total = weight / (height * height)
-    return total  # what goes here?
+def calculate_grade(score):
+    if score >= 90:
+        return("A")
+    elif score > 80 and score <= 89:
+        return("B")
+    elif score > 70 and score <= 79:
+        return("C")
+    elif score > 60 and score <= 69:
+        return("D")
+    else:
+        return("Fail")
 
-# outside - ask user for input
-weight = int(input("Please enter your weight: "))
-height = int(input("Please enter your height: "))
-
-# outside - call the function
-bmi = calculate_bmi(weight, height)  # you finish this
-
-# outside - check the result
-if bmi < 18.5:
-    print("Underweight")
-elif bmi < 22:  # you finish this
-    print("Healthy")
-else:
-      print("Overweight")  # you finish this    
+score = int(input("Please enter your score: "))
+Result = calculate_grade(score)
+print("Your grade is:" ,Result)
+                    
