64a220ed6c6f05b7f574f7a4ac2cf8c989a4bab0
jn10852 <junior.nfor@stud.th-deg.de>
2026-04-14 22:35:32 +0200

    Reading, sizing and showing an  videos  with OpenCV

--- OpenCVtest/test.py
+++ OpenCVtest/test.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 
+'''
 # Get the directory where this script is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
 # Build the path to the image file
@@ -10,4 +11,23 @@ mine = cv2.imread(image_path)
 mine = cv2.resize(mine, (600, 500))
 
 cv2.imshow("Jovi", mine)
-cv2.waitKey(0)
+cv2.waitKey(0) '''
+
+frameWidth = 360
+frameHeight = 640
+
+
+cap = cv2.VideoCapture("OpenCVtest\Resources\IMG_5789.MOV")  # (0) for webcam
+cap.set(3, frameWidth)  # 3 for default width
+cap.set(4, frameHeight)  # 4 for a default height
+
+
+# video to keep playing
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("video", img)
+    img = cv2.resize(img, (frameWidth, frameHeight))
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
