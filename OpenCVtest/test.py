80d4d5046cdee5aea133eacdb23a08dca36e5d60
jn10852 <junior.nfor@stud.th-deg.de>
2026-04-15 15:07:17 +0200

    Adjusting images colours

--- OpenCVtest/test.py
+++ OpenCVtest/test.py
@@ -1,7 +1,10 @@
 import cv2
 import os
+import numpy as np
 
 '''
+#for images
+
 # Get the directory where this script is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
 # Build the path to the image file
@@ -11,8 +14,9 @@ mine = cv2.imread(image_path)
 mine = cv2.resize(mine, (600, 500))
 
 cv2.imshow("Jovi", mine)
-cv2.waitKey(0) '''
+cv2.waitKey(0)
 
+#for videos
 frameWidth = 360
 frameHeight = 640
 
@@ -31,3 +35,25 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+'''
+kernel = np.ones((5, 5), np.uint8)
+# print(kernel)
+
+# Basic function (know different way to show the colour of an image)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(script_dir, "Resources", "IMG_3269.JPG")
+
+mine = cv2.imread(image_path)  # include 0 to be shown as gray colour
+mine = cv2.resize(mine, (600, 500))
+# showing the gray colour of the pic
+gray = cv2.cvtColor(mine, cv2.COLOR_BGR2GRAY)
+bluring = cv2.GaussianBlur(gray, (7, 7), 0)
+canny = cv2.Canny(bluring, 100, 100)
+dilation = cv2.dilate(canny, kernel, iterations=1)
+
+cv2.imshow("Laala", gray)
+cv2.imshow("Cann",  canny)
+cv2.imshow("Blur", bluring)
+cv2.imshow("mine", mine)
+cv2.imshow("lola", dilation)
+cv2.waitKey(0)
