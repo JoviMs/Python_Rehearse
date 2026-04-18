5f3254a555583fb165b60a6529af7ff368ac78b1
jn10852 <junior.nfor@stud.th-deg.de>
2026-04-18 23:26:28 +0200

    Initial commit with OpenCV and DeepFace setup, face detection with opencv

--- OpenCVtest/test.py
+++ OpenCVtest/test.py
@@ -1,5 +1,10 @@
 import cv2
 import os
+# import time
+# import uuid
+# from deepface import DeepFace
+# from matplotlib import pyplot as plt
+import pathlib
 
 '''
 #for images
@@ -56,7 +61,7 @@ cv2.imshow("Cann",  canny)
 cv2.imshow("Blur", bluring)
 cv2.imshow("mine", mine)
 cv2.imshow("lola", dilation)
-cv2.waitKey(0) '''
+cv2.waitKey(0) 
 
 # Cropping and Resizing images
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -71,4 +76,32 @@ imgCrop = img[300:440, 210:470]  # staying on lane
 
 cv2.imshow('Road', img)
 cv2.imshow('Roadcrop', imgCrop)
-cv2.waitKey(0)
+cv2.waitKey(0)  '''
+
+# face detection with opencv
+the_path = pathlib.Path(cv2.__file__).parent.absolute() / \
+    "data/haarcascade_frontalface_default.xml"
+clf = cv2.CascadeClassifier(str(the_path))
+# for webcam especially with 1 camera, if more, change to 1,2,3 etc,
+cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture("") # taking or capturing faces from a video
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = clf.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=3,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x, y, width, height) in faces:
+        cv2.rectangle(frame, (x, y), (x+width, y+height), (255, 255, 0), 2)
+
+    cv2.imshow("Faces", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
